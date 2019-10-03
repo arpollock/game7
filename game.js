9a88601feec85b8a5933b050d4d1172af1863a09
alexrpollock <arpollock@mines.edu>
2019-10-03 17:05:25 -0600

    Base JS interactions to start the game

--- game.js
+++ game.js
@@ -1,4 +1,43 @@
 var playing = false;
 
+function jump() {
+    alert("JUMP!!!");
+}
+
+function duck() {
+    alert("DUCK!!!");
+}
+
+function handleKeydown(event) {
+    if(event && event.keyCode) {
+        switch(event.keyCode){
+            case 38: // up
+                event.preventDefault(); // keep page from scrolling
+                if(playing) {
+                    jump();
+                }
+                break;
+            case 40: // down
+                event.preventDefault(); // keep page from scrolling
+                if(playing) {
+                    duck();
+                }
+                break;
+            case 32: // space
+                event.preventDefault(); // keep page from scrolling
+                if(!playing) {
+                    playing = true;
+                    $('#before_play').hide();
+                } else {
+                    jump();
+                }
+                break;
+            default: // ignore all other keys
+                break;
+        }
+    }
+}
+
 $(document).ready( function() {
+    $(this).keydown(handleKeydown);
 });
