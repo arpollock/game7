f8241e44af57d1984afcacccfb2ac13c9082283e
alexrpollock <arpollock@mines.edu>
2019-10-03 17:51:04 -0600

    Janky jump logic (will need to switch to update loop every second with acceleration, etc.)

--- game.js
+++ game.js
@@ -1,7 +1,43 @@
 var playing = false;
+var jumpHeight = 20;
+
+var canvas = document.getElementById('game');
+var ctx = canvas.getContext('2d');
+var groundY = 2 / 3;
+drawBackground();
+var img = new Image();
+var img_width = 0;
+var img_height = 0;
+var x = canvas.width / 5;
+var y = canvas.height * groundY;
+var MOVE_VAL = 5;
+
+img.onload = function () {
+    img_height = 40; //img.height / 2;
+    img_width = 40 / img.height * img.width;
+    // draw in center of canvas
+    y -= img_height;
+    drawHero();
+}
+
+function drawHero() {
+    ctx.drawImage(img, x, y, img_width, img_height);
+}
+
+function drawBackground() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgb(116, 125, 140)'; 
+    ctx.fillRect( 0, canvas.height * groundY, canvas.width, canvas.height * 1 / groundY);
+}
+
+img.src = './images/hero/test.png';
 
 function jump() {
-    alert("JUMP!!!");
+   if (y >= (groundY + img_height)) {
+        y -= jumpHeight;
+        drawBackground();
+        drawHero();
+   }
 }
 
 function duck() {
